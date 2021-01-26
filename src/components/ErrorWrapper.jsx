12cbc408de62084205eda3c8dd3658a7f9c6093b
Instance Noodle <ntn.f51@gmail.com>
2021-01-26 12:08:14 +0700

    add error boundaries

---
+++ src/components/ErrorWrapper.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return <h1>Oops! Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
